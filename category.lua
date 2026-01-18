b2509419c9e49ac47f6d9293e6e4af8d61aa6ace
ApoHikingSociety <dampiosrj@gmail.com>
2026-01-18 22:26:15 +0800

    Update category.lua

--- category.lua
+++ category.lua
@@ -2,22 +2,9 @@ return function(Modules)
     local Category = {}
     Category.__index = Category
 
-    function Category.new(name, parent)
+    function Category.new(name)
         local self = setmetatable({}, Category)
         self.Name = name
-
-        local frame = Instance.new("Frame")
-        frame.Name = name
-        frame.Size = UDim2.fromOffset(260, 180)
-        frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-        frame.BorderSizePixel = 0
-        frame.Parent = parent
-
-        local corner = Instance.new("UICorner")
-        corner.CornerRadius = UDim.new(0, 26)
-        corner.Parent = frame
-
-        self.Frame = frame
         return self
     end
 
